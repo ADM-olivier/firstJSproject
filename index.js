f4746df917cc863a0ee85574d4e2154ef6299085
mahoo <146084234+ADM-olivier@users.noreply.github.com>
2024-07-01 23:22:09 +0200

    we create our very first JavaScript file, using operators and comparision operators

---
+++ index.js
@@ -0,0 +1,54 @@
+// let x = 5
+// x +=x + 4
+// let g = 10
+// g +=4
+// console.log(x)
+// console.log(g)
+// console.log( g === x)
+
+
+let testexp = 60 < 70
+console.log(testexp)
+
+
+const result = 150;
+let historyResult = 51;
+let engResult = 72;
+let qartuliResult = 40;
+const testResult = historyResult + engResult + qartuliResult >= result;
+console.log( testResult)
+
+
+
+
+
+const results = 20;
+let xResult = 5;
+let yResults = 7;
+let zResults = xResult + yResults;
+console.log(zResults == yResults)
+
+
+
+// let x = 'mathResults'
+// let y = 'engResults'
+// const test = 'testResults'
+// testResults = 'mathResults' + 'engResults'
+// console.log(test)
+
+
+
+console.log(barier=120, math=60, history=72, test =math+history<=barier, test )
+
+
+
+// console.log(zResults ===testResults)
+
+
+
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+
+
+const results = 20
+let xResult = 5
+let yResults = 7
+let zResults = xResult + yResults
+console.log(zResults== xResult)
