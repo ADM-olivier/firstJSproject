33e76ae91ae2b8b77af8168edf2ebd31ca6e4dd0
mahoo <146084234+ADM-olivier@users.noreply.github.com>
2024-07-02 16:29:42 +0200

    we add couple comparison operators in you first java project

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-// let x = 5
-// x +=x + 4
-// let g = 10
-// g +=4
-// console.log(x)
-// console.log(g)
-// console.log( g === x)
+// // let x = 5
+// // x +=x + 4
+// // let g = 10
+// // g +=4
+// // console.log(x)
+// // console.log(g)
+// // console.log( g === x)
 
 
-let testexp = 60 < 70
-console.log(testexp)
+// let testexp = 60 < 70
+// console.log(testexp)
 
 
-const result = 150;
-let historyResult = 51;
-let engResult = 72;
-let qartuliResult = 40;
-const testResult = historyResult + engResult + qartuliResult >= result;
-console.log( testResult)
-
-
-
-
-
-const results = 20;
-let xResult = 5;
-let yResults = 7;
-let zResults = xResult + yResults;
-console.log(zResults == yResults)
-
-
-
-// let x = 'mathResults'
-// let y = 'engResults'
-// const test = 'testResults'
-// testResults = 'mathResults' + 'engResults'
-// console.log(test)
-
-
-
-console.log(barier=120, math=60, history=72, test =math+history<=barier, test )
-
-
-
-// console.log(zResults ===testResults)
+// const result = 150;
+// let historyResult = 51;
+// let engResult = 72;
+// let qartuliResult = 40;
+// const testResult = historyResult + engResult + qartuliResult >= result;
+// console.log( testResult)
 
 
 
 
 
+// const results = 20;
+// let xResult = 5;
+// let yResults = 7;
+// let zResults = xResult + yResults;
+// console.log(zResults == yResults)
+
+
+// console.log(barier=120, math=60, history=72, test =math+history>=barier, test )
+
+
+
+
+
+// const bar=165 
+// let x = 52
+// let y = 72
+// let e = 40
+// let tr = x+y+e
+
+// const pas = tr >= bar
+// console.log(tr,bar, tr>=bar,)
+// console.log('you failed due to one point')
+let buybutton = false
+const showBuyButtion = buybutton
+console.log(!buybutton)
+
+
+let agevote = 18
+let myage = 18
+const voteable = (myage>=agevote)
+console.log (voteable)
+
+
+
+
+
+
+  let x = 12
+  let y = '12'
+  console.log(x==y)
 
 
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,6 @@
 
-
-const results = 20
-let xResult = 5
-let yResults = 7
-let zResults = xResult + yResults
-console.log(zResults== xResult)
+let x = 'mathResults'
+let y = 'engResults'
+const test = 'testResults'
+testResults = 'mathResults' + 'engResults'
+console.log(test)
